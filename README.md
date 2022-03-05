d304c0a4ba8614fe1ab24946186cbda22558effd
Khaosingx <kennedyrecuperaodiscord@gmail.com>
2022-03-05 12:31:53 -0300

    calculatorimc

---
+++ README.md
@@ -0,0 +1,11 @@
+# Calculadora IMC
+
+Projeto em ReactJS + Typescript
+
+
+### Basic Installation
+- `npm install`
+
+### For run this project
+
+- `yarn dev` or others..
---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import { levels, calculateImc, Level } from "./helpers/imc";
+import { GridItem } from "./components/GridItem";
+import leftArrowImage from "./assets/leftarrow.png";
+
+function App() {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null>(null);
+
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+      setToShow(calculateImc(heightField, weightField));
+    } else {
+      alert("Digite todos os campos.");
+    }
+  };
+
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
+  };
+
+  return (
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img alt="" width={150} />
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p>
+            IMC é a singla para Índice de Massa Corpórea, parâmetro adotado pela
+            Organização Mundial de Saúde para calcular o peso ideal de cada
+            pessoa.
+          </p>
+
+          <input
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.5 (em métros)"
+            value={heightField > 0 ? heightField : ""}
+            onChange={(e) => setHeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
+          />
+
+          <input
+            type="number"
+            placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
+            value={weightField > 0 ? weightField : ""}
+            onChange={(e) => setWeightField(parseFloat(e.target.value))}
+            disabled={toShow ? true : false}
+          />
+
+          <button
+            onClick={handleCalculateButton}
+            disabled={toShow ? true : false}
+          >
+            Calcular
+          </button>
+        </div>
+
+        <div className={styles.rightSide}>
+          {!toShow && (
+            <div className={styles.grid}>
+              {levels.map((item, key) => (
+                <GridItem key={key} item={item} />
+              ))}
+            </div>
+          )}
+          {toShow && (
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25} />
+              </div>
+              <GridItem item={toShow} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
